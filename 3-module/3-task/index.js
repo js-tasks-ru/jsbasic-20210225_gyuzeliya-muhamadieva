1b69ba4e8907e88b9e57ed87856f9e9b3b5dc3dd
Gyuzeliya Mukhamadieva <ext.gyzeliya.mukhamadieva@tieto.com>
2021-03-22 19:28:09 +0300

    Fourth  task solution

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  if (!str) {
+    return str;
+  }
+
+  return str
+    .split('-')
+    .map((elem, idx) => idx === 0 ? elem : elem.charAt(0).toLocaleUpperCase() + elem.slice(1))
+    .join('');
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,7 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = str.split(/[,\s*]\s*/).map(el => parseFloat(el)).filter(el => !isNaN(el)).sort((a, b) => a - b);
+  return {
+    min: result[0],
+    max: result[result.length - 1]
+  };
 }
